a23b970380ceced917df1fd285f31451cb1cd834
tharinduDeakin <s224169428@deakin.edu.au>
2024-08-07 22:36:45 +1000

    db url

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://user:BISKgnK5sFOG1Uf489Xm1TWPh7wqCNXM@dpg-cqpmdcqj1k6c73dt3ki0-a.oregon-postgres.render.com/sit722postgre")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
